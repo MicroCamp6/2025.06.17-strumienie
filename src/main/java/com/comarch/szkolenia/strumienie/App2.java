4f870a8903fa657dd411db151a7cb6249a1916eb
Mateusz <mat.bereda@gmail.com>
2025-06-24 01:10:11 +0200

    update

--- src/main/java/com/comarch/szkolenia/strumienie/App2.java
+++ src/main/java/com/comarch/szkolenia/strumienie/App2.java
@@ -1,6 +1,8 @@
 package com.comarch.szkolenia.strumienie;
 
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.Stream;
 
 public class App2 {
     public static void main(String[] args) {
@@ -16,5 +18,37 @@ public class App2 {
                 .forEach(System.out::println);
 
         System.out.println(list);
+
+        List<Integer> list2 = List.of(1,2,3,4,5);
+
+        list2.stream()
+                .mapToInt(i -> i)
+                .average()
+                .ifPresent(System.out::println);
+
+        List<String> strings = List.of(
+                "janusz",
+                "jakis string",
+                "jak, asdf",
+                "asdOasd",
+                "asdASD asd",
+                "asdf.asd",
+                "asdf.asdf,asdf",
+                "Asdf.asdf",
+                "fasd.asdf asdf",
+                "asdf, asdf",
+                "asdASD,Aasdf"
+        );
+
+        double result = strings.stream()
+                .filter(s -> s.equals(s.toLowerCase()))
+                .filter(s -> !s.contains(" "))
+                .map(s -> s.replaceAll("[\\.,]", ""))
+                .mapToInt(String::length)
+                .average()
+                .getAsDouble();
+
+        System.out.println(result);
+
     }
 }
